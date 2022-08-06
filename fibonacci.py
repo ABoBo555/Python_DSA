537dff0cfae0e01eb2e4cb0c808b60f86a2455d7
Raj Kapoor <robbobroy224@gmail.com>
2022-08-06 22:24:23 +0630

    first commit org file of fibonacci and knapsack

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+import sys
+
+# fibonancci with divide and conquer using recursion
+def fib(num):
+    if num < 2:  # optimal sol:
+        return n
+    return fib(num - 1) + fib(num - 2)
+
+
+print(fib(8))
