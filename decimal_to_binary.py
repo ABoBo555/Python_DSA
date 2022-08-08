4cdbca9458a48084fcfe1b40303f5888cbef79b7
Raj Kapoor <robbobroy224@gmail.com>
2022-08-08 16:54:36 +0630

    #decimal_to_binary_problem

---
+++ decimal_to_binary.py
@@ -0,0 +1,18 @@
+from stack import Stack
+
+
+def to_binary(num):
+    binary_stack = Stack()
+    binary_code = ""
+    while num > 0:
+        print(num)
+        binary_stack.push(str(num % 2))
+        num = num // 2
+    while not binary_stack.is_empty():
+        binary_code += binary_stack.pop()
+
+    return binary_code
+
+
+print(to_binary(int(input("Enter decimal to covert binary code : "))))
+# print(int(to_binary(56), 2) == 56)
