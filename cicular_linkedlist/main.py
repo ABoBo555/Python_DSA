92d98463573f36d6353526fa06153fd87bdbbee1
AungBoBo <robbobroy224@gmail.com>
2022-08-12 17:21:15 +0630

    #additional functions

--- cicular_linkedlist/main.py
+++ cicular_linkedlist/main.py
@@ -5,6 +5,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        # print(self, self.data)
 
 
 class Linkedlist:
@@ -38,10 +39,44 @@ class Linkedlist:
     def print_list(self):
         temp_node = self.head
         while temp_node:
+            # print(temp_node.data, temp_node.next)
             print(temp_node.data, end=" ")
             temp_node = temp_node.next
             if temp_node == self.head:
                 break
+
+    def remove(self, key):
+        head_node = self.head
+        cur_node = self.head
+        prev_node = None
+
+        if key == self.head.data:
+            if self.head == self.head.next:
+                self.head = None
+            else:
+                while cur_node.next != self.head:
+                    cur_node = cur_node.next
+                cur_node.next = self.head.next
+                self.head = cur_node.next
+        else:
+            while cur_node:
+                if cur_node.data != key:
+                    prev_node = cur_node
+                    cur_node = cur_node.next
+                else:
+                    prev_node.next = cur_node.next
+                    cur_node = None
+                    break
+
+    def len(self):
+        node = self.head
+        count = 0
+        while node:
+            count += 1
+            node = node.next
+            if node == self.head:
+                break
+        print(count)
 
 
 llist = Linkedlist()
@@ -50,6 +85,14 @@ llist.append("B")
 llist.append("C")
 llist.append("D")
 llist.prepand("V")
+llist.prepand("P")
+
+llist.remove("P")
+llist.remove("B")
+llist.remove("D")
+
 
 llist.print_list()
-llist.print_list()
+
+print()
+llist.len()
